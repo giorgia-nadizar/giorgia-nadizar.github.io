6e5ccce4e22e666440f2446f619d6d0e37278185
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-02-10 12:21:05 +0100

    Update.

---
+++ src/components/Timeline.vue
@@ -0,0 +1,113 @@
+<template>
+  <!-- Section: Timeline -->
+  <section class="py-5">
+    <ul class="timeline-with-icons">
+      <li v-for="event in events" :key="event.id" class="timeline-item mb-5">
+        <span class="timeline-icon">
+          {{ event.icon }}
+        </span>
+
+        <h5 class="fw-bold">{{ event.title }}</h5>
+        <p class="text-muted mb-2 fw-bold">{{ event.date }}</p>
+        <div v-html="event.description"></div>
+      </li>
+    </ul>
+  </section>
+  <!-- Section: Timeline -->
+</template>
+
+<script>
+export default {
+  name: "Timeline",
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          icon: "🇳🇴",
+          title:
+            "Research Assistant at the Oslo Metropolitan University (Oslo)",
+          date: "March 2021 - August 2021",
+          description:
+            "<p> Working with <a target='_blank' href='https://www.nichele.eu/'>Prof. Stefano Nichele</a>.</p>",
+        },
+        {
+          id: 2,
+          icon: "🇳🇱",
+          title:
+            "Ph.D. Internship at Centrum Wiskunde & Informatica (Amsterdam)",
+          date: "November 2022 - January 2023",
+          description:
+            "<p> Working with <a target='_blank' href='https://marcovirgolin.github.io/'>Dr. Marco Virgolin</a>.</p>",
+        },
+        {
+          id: 3,
+          icon: "🇫🇷",
+          title: "Research Visit at ISAE-Supaero (Toulouse)",
+          date: "May 2023 - October 2023",
+          description:
+            "<p> Working with <a target='_blank' href='https://d9w.github.io/'>Dr. Dennis Wilson</a> supported by a SPECIES scholarship.</p>",
+        },
+        {
+          id: 4,
+          icon: "🇺🇸",
+          title:
+            "Research Visit at Massachusetts Institute of Technology (Boston)",
+          date: "January 2024",
+          description:
+            "<p> Working in the <a target='_blank' href='https://alfagroup.csail.mit.edu/'>ALFA group</a>.</p>",
+        },
+        {
+          id: 5,
+          icon: "🇫🇷",
+          title: "Research Visit at University of Toulouse Capitole (Toulouse)",
+          date: "January 2024",
+          description:
+            "<p> Working with <a target='_blank' href='https://www.irit.fr/~Sylvain.Cussat-Blanc/index_en.php'>Prof. Sylvain Cussat-Blanc</a>.</p>",
+        },
+        {
+          id: 6,
+          icon: "🎓",
+          title: "Ph.D. cum laude",
+          date: "27th January 2025",
+          description:
+            "<p>Ph.D. in Applied Data Science and Artificial Intelligence from the University of Trieste.</p> <p>Supervisor: <a target='_blank' href='https://medvet.inginf.units.it/'>Prof. Eric Medvet</a> </p> <p>Thesis title: <a target='_blank' href='/#/thesis'>Towards Bio-Inspired Interpretable Embodied Artificial Intelligence</a> </p>",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.timeline-with-icons {
+  border-left: 1px solid hsl(0, 0%, 90%);
+  position: relative;
+  list-style: none;
+}
+
+.timeline-with-icons .timeline-item {
+  position: relative;
+}
+
+.timeline-with-icons .timeline-item:after {
+  position: absolute;
+  display: block;
+  top: 0;
+}
+
+.timeline-with-icons .timeline-icon {
+  position: absolute;
+  left: -55px;
+  background-color: #daf5f5;
+  color: #12a39f;
+  border-style: solid;
+  border-color: #12a39f;
+  border-radius: 50%;
+  height: 31px;
+  width: 31px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/views/Biography.vue
+++ src/views/Biography.vue
@@ -1,40 +1,50 @@
 <template>
   <div class="biography">
     <h1 class="view-title">Biography</h1>
-      <p>
-        I am a third year PhD student in Applied Data Science and Artificial
-        Intelligence in the <a target="_blank" href="https://erallab.inginf.units.it/">"Evolutionary
-        Robotics and Artificial Life Lab"</a>,
-        <a target="_blank" href="https://www.units.it/">University of Trieste</a>,
-        working under the supervision of <a target="_blank" href="https://medvet.inginf.units.it/">Prof. Eric Medvet</a>.
-      </p>
+    <p>
+      I am a PostDoctoral Research Fellow in the
+      <a target="_blank" href="https://erallab.inginf.units.it/"
+        >"Evolutionary Robotics and Artificial Life Lab"</a
+      >,
+      <a target="_blank" href="https://www.units.it/">University of Trieste</a>,
+      working under the supervision of
+      <a target="_blank" href="https://medvet.inginf.units.it/"
+        >Prof. Eric Medvet</a
+      >
+      and
+      <a
+        target="_blank"
+        href="https://naturalcomputinglab.github.io/people/lmanzoni"
+        >Prof. Luca Manzoni</a
+      >.
+    </p>
 
-      <p>
-        My research interests lie at the intersection of embodied AI and explainable/interpretable AI.
-        Namely, my goal is to find robotic controllers which can be effective while remaining directly human intelligible.
-        My current approach to the problem relies on the use of different nuances of Genetic Programming,
-        but I am always open to explore new paths.
-      </p>
+    <p>
+      My research interests lie at the intersection of embodied AI and
+      explainable/interpretable AI. Namely, my goal is to find robotic
+      controllers which can be effective while remaining directly human
+      intelligible. My current approach to the problem relies on the use of
+      different nuances of Genetic Programming, but I am always open to explore
+      new paths.
+    </p>
 
-      <p>
-        My academic journey has allowed me to experience <a target="_blank" href="/research-trips">several research environments</a>.
-        At the end of 2022 I spent three months at the Centrum Wiskunde & Informatica in Amsterdam working
-        with <a target="_blank" href="https://marcovirgolin.github.io/">Dr. Marco Virgolin</a>, whereas
-        in 2023, I spent five months at the ISAE-Supaero in Toulouse to work with 
-        <a target="_blank" href="https://d9w.github.io/">Dr. Dennis Wilson</a>.
-        More recently, at the beginning of 2024 I spent one month at the Massachusetts Institute of Technology (MIT) in the USA, 
-        hosted in the <a target="_blank" href="https://alfagroup.csail.mit.edu/">ALFA group</a>.
-        Prior to my PhD position, I earned my Bachelor and Master in Electronics and Computer Engineering at
-        the University of Trieste, in 2019 and 2021 respectively.
-        During my Master Thesis, in 2021, I spent six months working as research assistant for the
-        Oslo Metropolitan University with <a target="_blank" href="https://www.nichele.eu/">Prof. Stefano Nichele</a>.
-      </p>
+    <p>
+      I earned my Ph.D. at the University of Trieste but my academic journey has
+      allowed me to experience
+      <a target="_blank" href="/research-trips">several research environments</a
+      >.
+    </p>
 
-    </div>
+    <Timeline />
+  </div>
 </template>
 
 <script>
+import Timeline from "@/components/Timeline.vue";
 export default {
-  title: 'Giorgia Nadizar'
-}
+  title: "Giorgia Nadizar",
+  components: {
+    Timeline,
+  },
+};
 </script>
