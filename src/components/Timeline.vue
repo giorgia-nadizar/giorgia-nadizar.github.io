b45d6870e3f6f95b3348e260ab02e9e179031372
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-11-04 16:21:11 +0100

    Update.

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -23,7 +23,7 @@ export default {
     return {
       events: [
         {
-          id: 10,
+          id: -10,
           icon: "🇫🇷",
           title:
             "Postdoctoral Researcher at the University of Toulouse Capitole (Toulouse)",
@@ -32,7 +32,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://www.irit.fr/~Sylvain.Cussat-Blanc/index_en.php'>Prof. Sylvain Cussat-Blanc</a>.</p>",
         },
         {
-          id: 9,
+          id: -9,
           icon: "🇨🇦",
           title: "Research Visit at McMaster University (Hamilton)",
           date: "June 2025",
@@ -40,7 +40,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://creativealgorithms-cd4c88.gitlab.io/team/'>Dr. Stephen Kelly</a>.</p>",
         },
         {
-          id: 8,
+          id: -8,
           icon: "🎓",
           title: "Ph.D. cum laude",
           date: "27th January 2025",
@@ -48,7 +48,7 @@ export default {
             "<p>Ph.D. in Applied Data Science and Artificial Intelligence from the University of Trieste.</p> <p>Supervisor: <a target='_blank' href='https://medvet.inginf.units.it/'>Prof. Eric Medvet</a> </p> <p>Thesis title: <a target='_blank' href='/#/thesis'>Towards Bio-Inspired Interpretable Embodied Artificial Intelligence</a> </p>",
         },
         {
-          id: 7,
+          id: -7,
           icon: "🇮🇹",
           title: "Postdoctoral Researcher at the University of Trieste",
           date: "November 2024 - October 2025",
@@ -56,7 +56,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://medvet.inginf.units.it/'>Prof. Eric Medvet</a>.</p>",
         },
         {
-          id: 6,
+          id: -6,
           icon: "🇫🇷",
           title:
             "Research Visit at the University of Toulouse Capitole (Toulouse)",
@@ -65,7 +65,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://www.irit.fr/~Sylvain.Cussat-Blanc/index_en.php'>Prof. Sylvain Cussat-Blanc</a>.</p>",
         },
         {
-          id: 5,
+          id: -5,
           icon: "🇺🇸",
           title:
             "Research Visit at Massachusetts Institute of Technology (Boston)",
@@ -74,7 +74,7 @@ export default {
             "<p> Working in the <a target='_blank' href='https://alfagroup.csail.mit.edu/'>ALFA group</a>.</p>",
         },
         {
-          id: 4,
+          id: -4,
           icon: "🇫🇷",
           title: "Research Visit at ISAE-Supaero (Toulouse)",
           date: "May 2023 - October 2023",
@@ -82,7 +82,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://d9w.github.io/'>Dr. Dennis Wilson</a> supported by a SPECIES scholarship.</p>",
         },
         {
-          id: 3,
+          id: -3,
           icon: "🇳🇱",
           title:
             "Ph.D. Internship at Centrum Wiskunde & Informatica (Amsterdam)",
@@ -91,7 +91,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://marcovirgolin.github.io/'>Dr. Marco Virgolin</a>.</p>",
         },
         {
-          id: 2,
+          id: -2,
           icon: "🇮🇹",
           title:
             "PhD Student in Applied Data Science & Artificial Intelligence at the University of Trieste",
@@ -100,7 +100,7 @@ export default {
             "<p> Working with <a target='_blank' href='https://medvet.inginf.units.it/'>Prof. Eric Medvet</a>.</p>",
         },
         {
-          id: 1,
+          id: -1,
           icon: "🇳🇴",
           title:
             "Research Assistant at the Oslo Metropolitan University (Oslo)",
